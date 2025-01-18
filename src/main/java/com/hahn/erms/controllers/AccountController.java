fa0c1279a68f2dd6a5fcc1999988216bc7998f63
YixxSTAR <zakaria.ziane14@gmail.com>
2025-01-18 16:49:50 +0100

    Feat: Account Controller

---
+++ src/main/java/com/hahn/erms/controllers/AccountController.java
@@ -0,0 +1,53 @@
+package com.hahn.erms.controllers;
+
+import com.hahn.erms.entities.Account;
+import com.hahn.erms.services.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/accounts")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return accountService.getAllAccounts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> getAccountById(@PathVariable Long id) {
+        Optional<Account> account = accountService.getAccountById(id);
+        return account.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> createAccount(@Valid  @RequestBody Account account) {
+        Account createdAccount = accountService.createAccount(account);
+        return new ResponseEntity<Account>(createdAccount, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Account> updateAccount(@PathVariable Long id, @RequestBody Account accountDetails) {
+        Account updatedAccount = accountService.updateAccount(id, accountDetails);
+        return ResponseEntity.ok(updatedAccount);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.noContent().build();
+    }
+}
